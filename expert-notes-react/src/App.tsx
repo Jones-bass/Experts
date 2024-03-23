3a934e43734b65035fa685b6a15c01dd82c2b734
jones-bass <jonesbass.tb@gmail.com>
2024-03-23 18:22:06 -0300

    Component Cards

--- expert-notes-react/src/App.tsx
+++ expert-notes-react/src/App.tsx
@@ -1,20 +1,25 @@
-import logo from "./assets/logo-nlw-expert.svg";
+import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
-    <img src={logo} alt="NLW Expert" />
+      <img src={logo} alt="NLW Expert" />
 
-    <form className="w-full">
-      <input
-        type="text"
-        placeholder="Busque em suas notas..."
-        className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
-      />
-    </form>
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+        />
+      </form>
 
-    <div className="h-px bg-slate-700" />
-
-  </div>
+      <div className="h-px bg-slate-700" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+      </div>
+    </div>
   )
 }
